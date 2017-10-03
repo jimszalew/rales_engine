e8cd4c73958f4bd993fff5f060dcf18a8f3d3a66
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-02 23:26:55 -0600

    customers record endpoints built

---
+++ spec/request/api/v1/customer_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Customer API', type: :request do
+  it 'returns all of customers' do
+    create_list(:customer, 3)
+
+    get '/api/v1/customers'
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+
+    expect(customers.count).to eq(3)
+  end
+
+  it 'returns a single customer by id' do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer["id"]).to eq(id)
+  end
+end
