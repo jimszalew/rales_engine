7df7d821a0888f07a27348661af51759f9c9cc6d
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-05 11:18:19 -0600

    add serializer to controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     merchant = Merchant.find(params[:id])
-    render json: merchant.find_revenue
+    render json: merchant.find_revenue, serializer: RevenueSerializer
   end
 end
