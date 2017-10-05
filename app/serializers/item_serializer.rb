6d4bad3ad0dca3bb25235b838315cde947109c6f
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-05 10:57:22 -0600

    adjust unit price to pass spec harness

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :description, :id, :merchant_id, :name, :unit_price
+
+  def unit_price
+    ((object.unit_price.to_i) / 100.00).to_s
+  end
 end
